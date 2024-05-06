aefe5df1405830883b367e3a0a0f30e810d235e7
Mohammed Alshaer <momo.alshaer@hord.app>
2024-05-06 23:09:19 +0300

    update build configs

--- packages/hooks/vite.config.ts
+++ packages/hooks/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import dts from "vite-plugin-dts";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,13 +13,31 @@ export default defineConfig({
     }),
   ],
   build: {
+    lib: {
+      //Defines the entry point for the library build. It resolves
+      //to src/index.ts,indicating that the library starts from this file.
+      entry: path.resolve(__dirname, "src/index.ts"),
+      name: "mo-hooks",
+      //A function that generates the output file
+      //name for different formats during the build
+      fileName: (format) => `index.${format}.js`,
+    },
     outDir: "dist",
     assetsDir: "", // Specify an empty assetsDir
     assetsInlineLimit: 0,
     rollupOptions: {
+      external: ["react", "react-dom"],
       output: {
-        entryFileNames: "index.js", // Output a single index.js file
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+        },
       },
     },
+    //Generates sourcemaps for the built files,
+    //aiding in debugging.
+    sourcemap: true,
+    //Clears the output directory before building.
+    emptyOutDir: true,
   },
 });
